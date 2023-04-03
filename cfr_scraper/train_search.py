c7f752d6e18fb69fd440f6c5facc55d8ff334200
Teodor Moscaliuc <teodormoscaliuc@outlook.com>
2023-04-03 16:36:42 +0300

    add data from dict to class Trip

---
+++ cfr_scraper/train_search.py
@@ -0,0 +1,7 @@
+import time
+class Trip:
+    def __init__(self,data):
+        self.start_city=data["dep"]["city"]
+        self.start_time=data["dep"]["time"]
+        self.finish_city=data["arrival"]["city"]
+    
