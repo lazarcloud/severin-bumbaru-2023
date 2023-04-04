4cb11aa25d31022f446bf1cb7f93cd704b0fcb5e
Teodor Moscaliuc <teodormoscaliuc@outlook.com>
2023-04-04 19:24:17 +0300

    input params for each of next 30 days

--- cfr_scraper/test2.py
+++ cfr_scraper/test2.py
@@ -1,9 +1,3 @@
-import itertools
-
-list1 = [1, 2, 3]
-list2 = [4, 5]
-list3 = [6, 7, 8]
-
-combinations = [combo for combo in itertools.product(list1, list2, list3) if combo[0] != combo[1]]
-
-print(combinations)
+from datetime import time, datetime
+midnight = datetime.combine(datetime.today(), time.min).timestamp
+print(midnight)
