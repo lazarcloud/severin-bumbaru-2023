0b9b84b7ed4e9df6e39f5220c5f6f2b4050cb353
AndyGooShooter <91827827+AndyGooShooter@users.noreply.github.com>
2023-04-04 12:42:40 +0300

    tried to add maps_api integration (failed-production cost); added handwriten db

---
+++ unitesting/maps/eta.js
@@ -0,0 +1,17 @@
+var axios = require('axios');
+
+var config = {
+  method: 'get',
+  url: 'https://maps.googleapis.com/maps/api/distancematrix/json?origins=Washington%2C%20DC&destinations=New%20York%20City%2C%20NY&units=imperial&key=YOUR_API_KEY',
+  headers: { }
+};
+
+axios(config)
+.then(function (response) {
+  console.log(JSON.stringify(response.data));
+})
+.catch(function (error) {
+  console.log(error);
+});
+
+//COSTA BANI
