f300163a5ae3c37f031b13d25873bf28e71dbe78
Teodor Moscaliuc <teodormoscaliuc@outlook.com>
2023-04-04 13:23:08 +0300

    q fix funts overnight and changes

--- cfr_scraper/test.py
+++ cfr_scraper/test.py
@@ -25,12 +25,32 @@ html = browser.page_source
 soup = BeautifulSoup(html, 'lxml')
 browser.close()
 
+def is_direct_from_duration(duration_span):
+    train_combs_div = duration_span.parent.find_previous_sibling('div')
+    if train_combs_div.find('i'):
+        return False
+    else: return True
+
+def is_night_train_from_duration(duration_span):
+    parent_card = duration_span.findParent('div',{'class' : 'row div-itineraries-row-main'})
+    if parent_card.find('div',{'class',"div-itineraries-station-time-part-added-time-sm mr-sm-1"}).find('span'):
+        return True
+    else:
+        return False
+
+#div-itinerary-station - class for cards
 trip_durations = soup.find_all('span', {'class' : 'd-inline-block'})
+"""trip_duration = soup.find('span', {'class' : 'd-inline-block'})"""
+
 
 for duration in trip_durations:
-
     minutes = duration.text.strip().split(" ")[-2]
     hours=0
     if "ore" in duration.text: hours = duration.text.strip().split(" ")[-4]
+    print(hours , " ", minutes)
+    print("Direct = ",is_direct_from_duration(duration))
+    print("Este tren de noapte? = ", is_night_train_from_duration(duration))
+   
 
-print(hours , " ", minutes)
+
+
