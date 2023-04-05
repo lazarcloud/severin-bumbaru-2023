1bc0bf19b7291523ace5d37df68f6a550b952587
AndyGooShooter <91827827+AndyGooShooter@users.noreply.github.com>
2023-04-05 08:30:48 +0300

    Fixed Drive ROUTE Bug; Fixed redis read

--- unit_testing/ROUTING/tests.js
+++ unit_testing/ROUTING/tests.js
@@ -1,6 +1,6 @@
 import {chart_driveROUTE} from './DRIVE.js';
-//import {chart_trainROUTE} from './CFR.js';
+import {chart_trainROUTE} from './CFR.js';
 
-console.log(chart_driveROUTE(0, 0, [ [1, 3], [2, 2], [3, 1], [4, 1] ]));
+//console.log(chart_driveROUTE(1, 0, [ [0, 1], [2, 1] ]));
 //console.log(chart_driveROUTE(0, 0, [[2, 3]]));
-//console.log(chart_trainROUTE(0, 0, 0, []));
+console.log(chart_trainROUTE(false, false, 0, 0, []));
