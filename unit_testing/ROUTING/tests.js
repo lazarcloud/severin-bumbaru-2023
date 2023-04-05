819621720972718feb41458b8098855b1845140f
AndyGooShooter <91827827+AndyGooShooter@users.noreply.github.com>
2023-04-05 12:57:18 +0300

    changes on train route

--- unit_testing/ROUTING/tests.js
+++ unit_testing/ROUTING/tests.js
@@ -3,4 +3,5 @@ import {chart_trainROUTE} from './CFR.js';
 
 //console.log(chart_driveROUTE(1, 0, [ [0, 1], [2, 1] ]));
 //console.log(chart_driveROUTE(0, 0, [[2, 3]]));
-console.log(chart_trainROUTE(false, false, 0, 0, []));
+console.log(chart_driveROUTE(0, 0, []));
+//console.log(chart_trainROUTE(false, false, 0, 0, [1, 1] [2, 1], [3, 1]));
