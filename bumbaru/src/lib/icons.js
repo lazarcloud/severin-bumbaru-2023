58b05986bbd9140ebff1e99f9d7fbf88447d5369
lazar <64156137+MonsieurLazar@users.noreply.github.com>
2023-04-05 12:29:53 +0300

    nu mai stiu

--- bumbaru/src/lib/icons.js
+++ bumbaru/src/lib/icons.js
@@ -49,3 +49,45 @@ const { minx, miny, maxx, maxy } = coords
     y /= n
     return {X: x, Y: y}
   }
+
+  export function getLines(juds){
+    //loop through jud
+    let lines = []
+    for (let i = 0; i < juds.length - 1; i ++){
+        let jud = juds[i]
+        let jud2 = juds[i + 1]
+        let judcoords = getMediumCoords(jud)
+        let jud2coords = getMediumCoords(jud2)
+        let judpro = interprete(judcoords.X, judcoords.Y)
+        let judpro2 = interprete(jud2coords.X, jud2coords.Y)
+        let line = {
+            X1: judcoords.X,
+            Y1: judcoords.Y,
+            X2: jud2coords.X,
+            Y2: jud2coords.Y
+        }
+        lines.push(line)
+    }
+    return lines
+    //js loop through data dict with key and value
+    for (const [key, value] of Object.entries(data)) {
+        let oras = value
+        let icon = {
+            type: 'text',
+            value: key,
+            X: getMediumCoords(key).X,
+            Y: getMediumCoords(key).Y
+        }
+        icons.push(icon)
+        for (const [key2, value2] of Object.entries(oras)) {
+            let icon = {
+                type: value2.type,
+                X: value2.X,
+                Y: value2.Y
+            }
+            icons.push(icon)
+        }
+
+    }
+    return icons
+}
